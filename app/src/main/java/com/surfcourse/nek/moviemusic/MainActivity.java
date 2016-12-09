60a380709f521ae549ce55aafcf79f6fb222080e
FeodorM <Feodor.Mann@gmail.com>
2016-12-09 17:59:26 +0300

    Добавлены кнопки на главном экране

--- app/src/main/java/com/surfcourse/nek/moviemusic/MainActivity.java
+++ app/src/main/java/com/surfcourse/nek/moviemusic/MainActivity.java
@@ -2,12 +2,32 @@ package com.surfcourse.nek.moviemusic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    findViewById(R.id.recent_btn).setOnClickListener(this);
+    findViewById(R.id.new_btn).setOnClickListener(this);
+    findViewById(R.id.best_btn).setOnClickListener(this);
+  }
+
+  @Override
+  public void onClick(View v) {
+    switch (v.getId()) {
+      case R.id.recent_btn: {
+        break;
+      }
+      case R.id.new_btn: {
+        break;
+      }
+      case R.id.best_btn: {
+        break;
+      }
     }
+  }
 }
